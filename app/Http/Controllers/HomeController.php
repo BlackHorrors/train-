e7370e6278bb327c421a466625331660363891d2
Roman <romandaviduc@gmail.com>
2018-04-10 18:18:32 +0300

    Show

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roma
+ * Date: 10.04.2018
+ * Time: 16:49
+ */
+
+namespace App\Http\Controllers;
+
+
+class HomeController
+{
+public function show(){
+    return view ('home');
+}
+}
---
+++ app/Http/Controllers/RequestFormController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roma
+ * Date: 10.04.2018
+ * Time: 16:58
+ */
+
+namespace App\Http\Controllers;
+
+
+class RequestFormController
+{
+    public function show()
+    {
+        return view("request-form");
+    }
+}
